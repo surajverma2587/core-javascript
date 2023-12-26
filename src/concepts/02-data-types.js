7033fcb9eb62e6b2abbc1c9d070775633aa7085a
surajverma2587 <surajverma2587@gmail.com>
2023-12-26 14:03:36 +0000

    wip

--- src/concepts/02-data-types.js
+++ src/concepts/02-data-types.js
@@ -19,14 +19,3 @@ console.log(result);
 // String Type
 const email = "bobsmith@email.com";
 console.log(email);
-
-// BigInt Type
-const transactions = BigInt(Number.MAX_SAFE_INTEGER) + 1000n;
-console.log(transactions);
-// 9007199254740991n = 2^53 - 1
-
-// Symbol Type
-const fooSymbol = Symbol("foo");
-console.log(fooSymbol);
-Symbol.for("foo"); // Symbol("foo")
-Symbol.keyFor(Symbol.for("foo")); // foo
