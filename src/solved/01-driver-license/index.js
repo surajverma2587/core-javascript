137ff1ccbc6325b3d233eba48971f75e5229af58
surajverma2587 <surajverma2587@gmail.com>
2023-12-27 12:23:49 +0000

    wip

--- src/solved/01-driver-license/index.js
+++ src/solved/01-driver-license/index.js
@@ -1,13 +1,11 @@
 export const driverLicense = (age) => {
-  if (!Number.isInteger(age)) {
+  if (age <= 0 || !Number.isInteger(age) || !age) {
     return "Please enter a valid age, your age should be a positive integer.";
-  } else if (age > 0 && age < 18) {
+  } else if (age < 18) {
     return "Sorry, you are not allowed to drive until you turn 18 years of age.";
   } else if (age >= 18 && age <= 65) {
     return "Great news, you are cleared to drive until the age of 65.";
-  } else if (age > 65) {
-    return "Attention, you need to apply for a senior's license by taking the senior's driving test.";
   } else {
-    return "Please enter a valid age, your age should be a positive integer.";
+    return "Attention, you need to apply for a senior's license by taking the senior's driving test.";
   }
 };
